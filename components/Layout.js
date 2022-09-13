b6526b223bab2bc41f85982065650e51d14fe913
kwangric <43795498+kwangric@users.noreply.github.com>
2022-09-13 06:15:46 -0400

    Remove bottom bar

--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,14 @@
 import Nav from './Nav'
 import styles from '../styles/Layout.module.css'
-import Box from '@mui/material/Box'
-import BottomNavigation from '@mui/material/BottomNavigation'
-import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 
 const Layout = ({ children }) => {
   return (
     <>
       <Nav />
       <div className={styles.container}>
-        <main className={styles.main}>
-          {children}</main>
+        <main className={styles.main}>{children}</main>
       </div>
-              <BottomNavigation
-                showLabels
-                onChange={() => {
-                  window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth',
-                  })
-                }}
-              >
-                <BottomNavigationAction label="Back to top" />
-              </BottomNavigation>
+      <br />
     </>
   )
 }
