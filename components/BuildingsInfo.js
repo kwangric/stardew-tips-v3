739b43abc6a2e61bf820834544e95a81da0a2f42
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2023-11-08 16:45:01 +0000

    Add location icon and color

--- components/BuildingsInfo.js
+++ components/BuildingsInfo.js
@@ -42,61 +42,79 @@ const BuildingsInfo = () => {
                                             className="card"
                                             variant="outlined"
                                             sx={{
-                                                maxWidth: '350px',
+                                                width: '300px',
+                                                height: '425px',
                                                 display: 'flex',
                                                 flexDirection: 'column',
                                                 alignContent: 'flex-start',
                                                 justifyContent: 'space-between',
                                             }}
                                             style={{
-                                                backgroundColor: '#fdfd96',
+                                                backgroundColor: `${building.type === 'carpenter' ? '#fdfd96' : '#c3bbd7'}`,
                                                 borderRadius: '25px',
                                             }}
                                         >
-                                            <Box>
-                                                <Box className="card-title" position="relative">
+                                            <Box className="card-title" position="relative">
+                                                <CardMedia
+                                                    component="img"
+                                                    image={`/images/buildings/${building.imageUrl}.png`}
+                                                    sx={{
+                                                        padding: '1rem',
+                                                        width: 'auto',
+                                                        maxHeight: '200px',
+                                                    }}
+                                                    alt={building.imageUrl}
+                                                />
+                                                <Typography variant="h5" component="div">
+                                                    {building.name}
+                                                </Typography>
+                                                <Typography
+                                                    sx={{ fontSize: '0.75rem', fontWeight: 'bold' }}
+                                                    variant="subtitle2"
+                                                >
+                                                    {building.description}
+                                                </Typography>
+                                            </Box>
+                                            <Box
+                                                position="relative"
+                                                sx={{
+                                                    marginTop: '1rem',
+                                                }}>
+                                                {Object.entries(building.cost).map((cost, index) => {
+                                                    return (
+                                                        <Box key={index}>
+                                                            <CardMedia
+                                                                component="img"
+                                                                sx={{
+                                                                    width: 20,
+                                                                    height: 20,
+                                                                    display: 'inline',
+                                                                    verticalAlign: 'middle'
+                                                                }}
+                                                                image={`/images/resources/${cost[0]}.png`}
+                                                                alt='gold cost'
+                                                            />
+                                                            <Typography variant="body1" sx={{ display: 'inline' }}>
+                                                                {cost[1]}
+                                                            </Typography>
+                                                        </Box>
+                                                    )
+                                                })}
+                                                <Tooltip title={building.type === "carpenter" ? "Carpenter's Shop" : "Wizard's Tower"}>
                                                     <CardMedia
                                                         component="img"
-                                                        image={`/images/buildings/${building.imageUrl}.png`}
                                                         sx={{
-                                                            padding: '1rem',
-                                                            width: 'auto',
-                                                            maxHeight: 250,
+                                                            width: 25,
+                                                            height: 25,
+                                                            position: 'absolute',
+                                                            borderRadius: 5,
+                                                            bottom: 0,
+                                                            right: 0,
                                                         }}
-                                                        alt={building.imageUrl}
+                                                        image={`/images/buildings/${building.type}.png`}
+                                                        alt={building.type}
                                                     />
-                                                    <Typography variant="h5" component="div">
-                                                        {building.name}
-                                                    </Typography>
-                                                    <Typography
-                                                        sx={{ fontSize: '0.75rem', fontWeight: 'bold' }}
-                                                        variant="subtitle2"
-                                                    >
-                                                        {building.description}
-                                                    </Typography>
-                                                    <Box>
-                                                        {Object.entries(building.cost).map((cost, index) => {
-                                                            return (
-                                                                <Box key={index}>
-                                                                    <CardMedia
-                                                                        component="img"
-                                                                        sx={{
-                                                                            width: 20,
-                                                                            height: 20,
-                                                                            display: 'inline',
-                                                                            verticalAlign: 'middle'
-                                                                        }}
-                                                                        image={`/images/resources/${cost[0]}.png`}
-                                                                        alt='gold cost'
-                                                                    />
-                                                                    <Typography variant="body1" sx={{ display: 'inline' }}>
-                                                                        {cost[1]}
-                                                                    </Typography>
-                                                                </Box>
-                                                            )
-                                                        })}
-                                                    </Box>
-                                                </Box>
+                                                </Tooltip>
                                             </Box>
                                         </Card>
                                     </Grid>
