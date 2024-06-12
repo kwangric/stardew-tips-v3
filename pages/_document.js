35bd75e4807ff991d282dd232dd10ef382539299
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2024-06-12 13:18:10 +0000

    Revert "Set up GA?"
    This reverts commit 1b56bc8172893e25c6b6ebf8e98bee1e9e9d23a3.

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,15 @@
 /* eslint-disable react/no-unknown-property */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet" />
         </Head>
-        <GoogleAnalytics gaId="G-X3292HYLJE" />
         <body>
           <Main />
           <NextScript />
